8a6c0ba3d15b04974c15f4521837f532db71365b
arthuralvesx <arthuralvesbarbosa5560@gmail.com>
2023-06-13 15:09:53 -0300

    teste 111

--- JavaOrientacaoObjeto/src/Aula2/Ex01aula.java
+++ JavaOrientacaoObjeto/src/Aula2/Ex01aula.java
@@ -3,7 +3,11 @@ package Aula2;
 public class Ex01aula {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		
+
+
 
 	}
 
--- JavaOrientacaoObjeto/src/Aula2/ExFatorial.java
+++ JavaOrientacaoObjeto/src/Aula2/ExFatorial.java
@@ -6,4 +6,13 @@ public class ExFatorial {
 
 	public static void main(String[] args) {
 		
+		int fatorial = 1, valor = 1;
+		
+		for (int i = 1; i <= valor; i++) {
+			fatorial *= i;
+		}
+		
+		System.out.println(valor + "! = " +fatorial);	
+	}
+}		
 		
